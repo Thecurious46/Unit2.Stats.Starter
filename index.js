22b3a92545adbb718b018cfcce8bd178803543de
Lara Mustafa <Laramustafa006@gmail.com>
2023-10-13 18:21:13 -0700

    added changes

--- index.js
+++ index.js
@@ -25,12 +25,39 @@ for (let i = 0; i < stringArray.length; i++) {
 
 // Peform some calculations on the numbers.
 console.log(numbers);
+const numbers = [1,2,3,4,5]
+cont length = math.length(numbers)
 console.log(`You have given ${getLength(numbers: number[])} numbers.`);
+
+const numbers = [1,2,3,4,5]
+const sum = Math.sum(numbers)
 console.log(`The sum of your numbers is ${getSum(numbers: number[])}.`);
+
+const numbers = [1,2,3,4,5]
+const Math.mean(numbers)
 console.log(`The mean of your numbers is ${getMean(numbers: number[])}.`);
+
+const numbers = [1,2,3,4,5]
+const min = Math.min(numbers)
 console.log(`The smallest of your numbers is ${getMin(numbers: number[])}.`);
+
+const numbers = [1,2,3,4,5]
+const max = Math.max(numbers)
 console.log(`The largest of your numbers is ${getMax(numbers: number[])}.`);
+
+const numbers = [1,2,3,4,5]
+const range = Math.range(numbers)
 console.log(`The range of your numbers is ${getRange(numbers: number[])}.`);
+
+if (num %2 == 0){
+  is odd = 'number is odd'
+  console.log(isodd)
+}
+  else{
+    is odd = 'number is even'
+    cpnsole.log('iseven')
+  }
+
 console.log(`The even numbers you gave are ${getEvens(numbers: number[])}.`);
 console.log(`The odd numbers you gave are ${getOdds(numbers: number[-])}.`);
 
@@ -55,6 +82,11 @@ function getSum(numbers) {
 
 /**
  * @param {number[]} numbers an array of integers
+ * let numlength = num.length;
+ * console.log(numlength)
+ * 
+ * let nummean = num.mean;
+ * console.log (nummean)
  * @returns {number} the mean of the numbers
  */
 function getMean(numbers) {
